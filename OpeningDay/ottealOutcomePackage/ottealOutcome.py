8136e9d26df8d837497061030adba945c261b2a2
otteal <otteal@LCBLAB2110-46.uccob.uc.edu>
2023-03-30 08:31:35 -0400

    AO - Created function for answer

--- OpeningDay/ottealOutcomePackage/ottealOutcome.py
+++ OpeningDay/ottealOutcomePackage/ottealOutcome.py
@@ -1,1 +1,5 @@
-print("Hey Prof! I think the Reds will win today... they need this win...")
+
+def otteal_Answer():
+    print("Hey Prof! I think the Reds will win today... they need this win...")
+    
+otteal_Answer()
