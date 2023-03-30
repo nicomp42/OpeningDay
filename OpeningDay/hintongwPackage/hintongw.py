91af72e87c2554e80c8c7e35fa2ab8b0a7273919
hintongw <hintongw@LCBLAB2110-21.uccob.uc.edu>
2023-03-30 08:36:26 -0400

    Gabe's second commit

--- OpeningDay/hintongwPackage/hintongw.py
+++ OpeningDay/hintongwPackage/hintongw.py
@@ -1,2 +1,2 @@
 def hintogwfunction():
-    print("The Reds will have a record under .500 this year.")
+    print("The Reds will have a record under .500 this year and be worse than the Guardians.")
