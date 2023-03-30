97a6e3dde4b1c0e392eac5f07586072c9ffd8bcd
Halverhc <127410175+Halverhc@users.noreply.github.com>
2023-03-30 08:37:45 -0400

    Add files via upload

---
+++ OpeningDay/Halverhcs_SuperCoolPackage/Halverhcs_RedsPrediction_Module.py
@@ -0,0 +1,11 @@
+#Name: Henry Halverson
+#email: halverhc@mail.uc.edu
+#Assignment Title: In Class 3-30-2023
+#Course: IS 4010
+#Semester/Year: Spring 2023
+#Brief Description: Reds Prediction Text
+#Citations: #https://github.com/nicomp42/OpeningDay
+#Anything else that's relevant:
+# IS4010-002 Spring 2023
+
+print("The Reds will seize the day!")
