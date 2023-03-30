a4f4c5e11c046403c9e9d407832862a46bb2f282
Luke Brooks <brooksl7@mail.uc.edu>
2023-03-30 13:19:19 -0400

    Luke's Commit

--- OpeningDay/functionPackage/function.py
+++ OpeningDay/functionPackage/function.py
@@ -1,4 +1,5 @@
 # function.py
 
-redscore = "The Reds will have less than 100 losses"
-print(redscore)
+def reds_result():
+    redscore = "The Reds will have less than 100 losses"
+    print(redscore)
