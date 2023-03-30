da24342964b5967a3085d1b98984cdcfe409b99e
marti6mr <marti6mr@LCBLAB3120J-10.uccob.uc.edu>
2023-03-30 13:06:18 -0400

    New function updated

--- OpeningDay/redsResultPackage/redsResult.py
+++ OpeningDay/redsResultPackage/redsResult.py
@@ -3,7 +3,7 @@ import random
 def redsSeason():
     losses = random.randint(75,100)
     wins = 100 - losses
-    if losses < 90:
+    if losses < 85:
         print("The Reds will lose more than " + str(losses) + "% of their games this season")
     else:
         print("The Reds will at least win " + str(wins) + "% of their games this season")
